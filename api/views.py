4218186f67f356a707969c21a89e823cf41082a2
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-04-09 02:20:40 +0600

    auth restricted

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
-# serializers 
 from .serializers import RegisterUserSerializer
+from bidding.serializers import BiddingSerializer
 
-from django.contrib.auth.models import User
+from bidding.models import Bid
 from rest_framework import viewsets
 from rest_framework.permissions import (
 	IsAuthenticated, 
@@ -13,15 +13,17 @@ from rest_framework.permissions import (
 
 
 # Customer creation, edition, deletion through viewset
-class UserViewset(viewsets.ModelViewSet):
-	serializer_class = RegisterUserSerializer
-	queryset = User.objects.all()
+class BiddingViewset(viewsets.ModelViewSet):
+	serializer_class = BiddingSerializer
+	queryset = Bid.objects.all()
 	# queryset = Group.objects.get(name="CUSTOMER").user_set.all()
 
 	def get_permissions(self):
-		if self.request.method == 'POST':
+		if self.request.method == 'GET':
 			self.permission_classes = [AllowAny, ]
 		else:
-			self.permission_classes = [IsAdminUser, ]
-		return super(UserViewset, self).get_permissions()
-
+			self.permission_classes = [IsAdminUser, IsBidOwner]
+		return super(BiddingViewset, self).get_permissions()
+class IsBidOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.bidder == request.user
--- bidding/serializers.py
+++ bidding/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product, Bid
 
-class BiddingSerializers(serializers.ModelSerializer):
+class BiddingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bid
         fields = ('product', 'bidder', 'amount')
